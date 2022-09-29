caca3cf1e0a86ce88ba9fc10cf711719024222c6
APTEMmm <artemqrtz@gmail.com>
2022-09-29 12:34:47 +0300

    Updated code style

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -3,8 +3,8 @@ require 'colorize'
 class ConsoleInterface
   FIGURES =
     Dir["#{__dir__}/../data/figures/*.txt"]
-      .sort
-      .map { |file_name| File.read(file_name) }
+    .sort
+    .map { |file_name| File.read(file_name) }
 
   def initialize(game)
     @game = game
@@ -17,7 +17,7 @@ class ConsoleInterface
     puts "Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)
     puts <<~END
       У вас осталось ошибок: #{@game.errors_allowed}
-    
+
 
     END
     if @game.won?
